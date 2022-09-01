cba459566db7ba594525666603301718940950b5
Bogdan Toader <86833028+BogdanToader25@users.noreply.github.com>
2022-09-01 10:33:51 +0300

    Modified script index page script location and changed the item index view to use the partial view to not have duplicate code

--- PageChangeWithoutRefreshApp/wwwroot/js/Item/Index.js
+++ PageChangeWithoutRefreshApp/wwwroot/js/Item/Index.js
@@ -6,7 +6,7 @@
 
 function GetFilteredItems() {
     const container = document.getElementById("container");
-    postData('Item/GetFilteredItems/1')
+    postData('Item/GetFilteredItems/1') //I want to get items with the MyProperty3 value bigger than 1
         .then((data) => {
             container.innerHTML = data;
         });
